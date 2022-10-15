fd4070ac6d706aacf1de7e10bcb1aeb2241ca696
Galina <pgv21@yandex.ru>
2022-10-15 22:57:42 +0300

    save5

---
+++ Seminar2Task4.py
@@ -0,0 +1,12 @@
+from random import randint
+
+n = int(input("Введите число n: "))
+dict = {}
+multiply = 1
+for i in range(1, n+1):
+    dict[i] = randint(-n, n)
+print(dict)
+i = int(input("Введите позицию первого элемента от 1 до n: "))
+j = int(input("Введите позицию второго элемента от 1 до n: "))
+multiply = int(dict[i]*dict[j])
+print(f"Произведение элементов: {multiply}")
