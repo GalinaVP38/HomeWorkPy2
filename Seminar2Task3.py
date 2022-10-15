a7913e8418c8b0825329f733a7957801f2ade7dd
Galina <pgv21@yandex.ru>
2022-10-15 22:25:06 +0300

    save3

---
+++ Seminar2Task3.py
@@ -0,0 +1,11 @@
+from random import Random, random
+
+
+n = int(input("Введите число n:"))
+dict = {}
+sum = 0
+for i in range(1, n+1):
+    dict[i] = round((1+1/i)**i, 2)
+    sum+=float(dict[i])
+print(f"Список: {dict}")
+print(f"Сумма чисел: {sum}")
