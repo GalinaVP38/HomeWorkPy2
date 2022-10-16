45d4ee65afa738afe0bcc469e594432ef55641c1
Galina <pgv21@yandex.ru>
2022-10-16 11:38:27 +0300

    save

--- Seminar2Task4.py
+++ Seminar2Task4.py
@@ -8,5 +8,7 @@ for i in range(1, n+1):
 print(dict)
 i = int(input("Введите позицию первого элемента от 1 до n: "))
 j = int(input("Введите позицию второго элемента от 1 до n: "))
-multiply = int(dict[i]*dict[j])
-print(f"Произведение элементов: {multiply}")
+if (0<=i<=n) and (0<=j<=n):
+    multiply = int(dict[i]*dict[j])
+    print(f"Произведение элементов: {multiply}")
+else: print("Введены некорректные позиции элементов")
--- Seminar2Task5.py
+++ Seminar2Task5.py
@@ -1,7 +1,11 @@
+from random import randint
+
 list = [159 , 152 , 140 , 128 , 113]
 temp = 0
-for i in range(3):
+n=randint(0, 3)
+for i in range(n, 4):
     temp = list[i]
     list[i]=list[i+1]
     list[i+1]=temp
+print(n)
 print(list)
